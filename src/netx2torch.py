56f88b50612b1d55a2d3c16f19575cd6db946069
Joshua Zhou <joshuazhou03@gmail.com>
2025-04-21 16:27:55 -0500

    edited features to save to pytorch graphs to include electronegativity environment calculations

--- src/netx2torch.py
+++ src/netx2torch.py
@@ -5,6 +5,12 @@ import networkx as nx
 import torch
 import numpy as np
 from torch_geometric.data import Data
+from src.smiletovectors import get_full_neighbor_vectors
+from src.paulingelectro import get_eleneg_diff_mat
+from rdkit import Chem
+
+EN_MAT = get_eleneg_diff_mat()
+au2eV = 27.21139
 
 ### Helper functions from hackathon
 def load_data_from_file(filename) -> dict:
@@ -21,6 +27,34 @@ def _load_data_from_string_dict(string_dict) -> dict:
 		graph = nx.node_link_graph(string_dict[key], edges="edges")
 		result_dict[key] = graph
 	return result_dict
+
+def get_l(l):
+    '''
+    l = s, p, d, f, g
+    '''
+    return {'s': 0, 'p': 1, 'd': 2, 'f': 3, 'g':4,}.get(l, "l value is not valid")
+
+def get_n_l(orb):
+    n = int(orb[0])
+    l = get_l(str(orb[1]))
+    return n, l
+
+def giveorbitalenergy(ele, orb, orbital_energy_file='orbitalenergy.json'):
+    '''
+    For a given element and orbital, return the orbital energy in eV
+    '''
+    with open(orbital_energy_file, 'r') as f:
+        data = json.load(f)
+    try:
+        orbenegele = data[ele]
+        del data
+    except KeyError:
+        raise KeyError("Element symbol not found")
+    
+    n, l = get_n_l(orb)
+    cbenergy = orbenegele[str(l)][n-l-1]
+    cbenergy *= au2eV
+    return cbenergy
 
 ### Custom functions for processing the given data into pytorch-friendly format
 def process_edge(graph):
@@ -44,57 +78,87 @@ def process_edge(graph):
         edge_attr = None
     return edge_index, edge_attr
 
-def process_nodes(node_raw_data):
+def process_nodes(node_raw_data, vectors):
     node_data = []
     dup_orbs = {}
-    for node_idx, data in enumerate(node_raw_data):
-        orbitals = data['orbitals']
+    temp_list = []
+    for n in node_raw_data:
+        if n['atom_type'] == 'H':
+            continue
+        temp_list += [n]
+    node_data_no_H = tuple(temp_list)
+
+    assert len(vectors) == len(node_data_no_H), f'{len(vectors)} != {len(node_data_no_H)}'
+    for node_idx, data in enumerate(zip(vectors, node_data_no_H)):
+        v, n = data
+        idx, symbol, cmat = v
+    
+        lmat = np.zeros((1,EN_MAT.shape[0]))
+        lmat[0, Chem.Atom(symbol).GetAtomicNum() - 1] = 1
+        
+        e_neg_score = np.einsum('ij,jk,ki->i', lmat, EN_MAT, cmat)[0]        
+        
+        orbitals = n['orbitals']
         for i, orb in enumerate(orbitals):
             if type(orb) == int and orb == -1:
-                orbitals[i] = np.NaN
+                orbitals[i] = None
         
-        binding_energies = data['binding_energies']
+        binding_energies = n['binding_energies']
         for i, energy in enumerate(binding_energies):
             if energy == -1:
-                binding_energies[i] = np.NaN
+                binding_energies[i] = None
                 
-        atom_type = data['atom_type']
-        formal_charge = data['formal_charge']
+        symbol = n['atom_type']
+        formal_charge = n['formal_charge']
+        
+        # Remove empty entries
+        orbitals = np.array(orbitals)
+        orbitals = np.atleast_1d(orbitals[orbitals !=  None])
+        if len(orbitals) == 0:
+            return None, None
+        
+        binding_energies = np.array(binding_energies)
+        binding_energies = binding_energies[binding_energies != None]
+        if len(binding_energies) == 0:
+            return None, None
         
         # If there are duplicate orbitals
-        if len(orbitals) > 1:
-            orbitals = np.array(orbitals)
-            orbitals = orbitals[orbitals != 'nan']
-            if len(orbitals) == 0:
-                orbitals = ['nan']
-            
-            binding_energies = np.array(binding_energies)
-            binding_energies = binding_energies[~np.isnan(binding_energies)]
-            if len(binding_energies) == 0:
-                binding_energies = [np.NaN]
-            
+        if len(orbitals) > 1:            
             dup_orbs[node_idx] = []
-            for orb, energy in zip(orbitals, binding_energies):
-                dup_orbs[node_idx] += [[atom_type, formal_charge, orb, energy]]
-        node_data += [[atom_type, formal_charge, orbitals[0], binding_energies[0]]]
+            for orb, exp_e in zip(orbitals, binding_energies):
+                ref_e = -giveorbitalenergy(symbol, orb)
+                n, l = get_n_l(orb)
+                dup_orbs[node_idx] += [np.array([Chem.Atom(symbol).GetAtomicNum(), formal_charge, e_neg_score, n, l, ref_e, exp_e])]
+
+        n, l = get_n_l(orbitals[0])
+        ref_e = -giveorbitalenergy(symbol, orbitals[0])
+        node_data += [np.array([Chem.Atom(symbol).GetAtomicNum(), formal_charge, e_neg_score, n, l, ref_e, binding_energies[0]])]
     node_data = np.atleast_2d(node_data)
     return node_data, dup_orbs
 
 def networkx2torch(data):
     smiles = list(data.keys())
-    
     # For each networkx graph, process the nodes and edges into torch_geometric.data.Data graphs
     torch_graphs = []
     for graph_num, smile in enumerate(smiles):
         graph = data[smile]
+        try:
+            vectors = get_full_neighbor_vectors(smile)
+        except:
+            print(f"Skipping smile {smile} due to RDKit error")
+            continue
+
         print(f"Processing graph {graph_num+1}/{len(smiles)}")
+        
         ### Process Edge information  
         edge_index, edge_attr = process_edge(graph)
 
         ### Process Node information
         nodes, node_raw_data = zip(*graph.nodes(data=True))
-        node_data, dup_orbs = process_nodes(node_raw_data)
-
+        node_data, dup_orbs = process_nodes(node_raw_data, vectors)
+        if node_data is None or dup_orbs is None:
+            continue
+        
         # If any node has multiple orbital/binding_energy pairs
         if len(list(dup_orbs.keys())) > 0:
             # For each possible combination of nodes with variable orbital/binding_energies
